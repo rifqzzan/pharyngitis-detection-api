5eb5c73959ad24b7c8573044a8540388e3cbcd5a
rifqzzan <erl.warrior4@gmail.com>
2026-04-25 16:12:06 +0700

    fix: add CORS middleware

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from predictor import predict_image
 from llm import generate_explanation
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
